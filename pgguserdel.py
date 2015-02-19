4fe445d1b5de0b3e0717379d2d72098218416029
wcmckee <will@artcontrol.me>
2015-02-20 10:55:32 +1300

    blah

--- pgguserdel.py
+++ pgguserdel.py
@@ -1,31 +1,32 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[13]:
 
 import os
 import getpass
 from passlib.hash import pbkdf2_sha256
 import crypt, getpass, spwd
+from Crypto.PublicKey import RSA
 
 
-# In[2]:
+# In[14]:
 
 fuliz = os.listdir('/home')
 
 
-# In[3]:
+# In[15]:
 
 fuliz
 
 
-# In[4]:
+# In[16]:
 
 #Chabge password for user
 #pasuz = raw_input('User to change password: ')
 
 
-# In[5]:
+# In[17]:
 
 #logtest = getpass.getpass('new password: ')
 
@@ -33,7 +34,7 @@ fuliz
 #vercryp = pbkdf2_sha256.verify(logtest, hashez)
 
 
-# In[6]:
+# In[18]:
 
 #Enter user to delete.
 #Even better, user to lock.
@@ -43,7 +44,7 @@ fuliz
 #os.system('sudo passwd -l ' + locusa) 
 
 
-# In[7]:
+# In[19]:
 
 #Read hashed passwords from /etc/shadow
 #opshad = open('/etc/shadow', 'r')
@@ -55,12 +56,12 @@ fuliz
 #the password returned from spwd
 
 
-# In[8]:
+# In[20]:
 
-pan = 'wcmckee'
+pan = raw_input("Enter Username to delete: ")
 
 
-# In[9]:
+# In[21]:
 
 #enc_pwd = spwd.getspnam(pan)[1]
 #if enc_pwd in ["NP", "!", "", None]:
@@ -71,17 +72,17 @@ pan = 'wcmckee'
 #    print "password has expired"
 
 
-# In[14]:
+# In[22]:
 
 gpas = getpass.getpass('Enter Username Password: ')
 
 
-# In[21]:
+# In[32]:
 
 encpass = spwd.getspnam(pan)[1]
 
 
-# In[20]:
+# In[33]:
 
 if crypt.crypt(gpas, encpass) == encpass:
     print ('True')
@@ -89,37 +90,155 @@ else:
     print "incorrect password"
 
 
-# In[10]:
+# In[34]:
 
-print spwd.getspnam(pan)[1]
+#print spwd.getspnam(pan)[1]
 
 
-# In[11]:
+# In[35]:
 
 #shpa = spwd.getspnam('wcmckee')[1]
 
 
-# In[12]:
+# In[36]:
 
 #spwd.getspall()
 
 
-# In[13]:
+# In[37]:
 
 #shpa
 
 
-# In[13]:
+# rsa key generated for each user and stored in their
+# /home/user/.ssh/ folder. Public key is emailed, added to test servers.
+
+# In[38]:
+
+new_key = RSA.generate(2048, e=65537)
+public_key = new_key.publickey().exportKey("PEM")
+private_key = new_key.exportKey("PEM")
+print private_key 
+sapriv = open('/home/wcmckee/.ssh/' + pan, 'w')
+sapriv.write(private_key)
+sapriv.close()
+
+print public_key 
+papriv = open('/home/wcmckee/.ssh/' + pan + '.pub', 'w')
+papriv.write(public_key)
+papriv.close()
 
 
 
 
-# In[13]:
+# In[39]:
+
+#Spin up digital ocean server, with public key and 
+#user created.
 
 
+# In[1]:
+
+#import digitalocean
+
+
+# In[2]:
+
+#tok  = ('c54ea484dcf55053743215cdb37309cb77a153e9810f35851b4701d4c8bf2881')
+
+
+# In[3]:
+
+#digid = digitalocean.Manager(token='c54ea484dcf55053743215cdb37309cb77a153e9810f35851b4701d4c8bf2881')
+
+
+
+# In[4]:
+
+#digid.get_account
+
+
+# In[15]:
+
+#my_droplets = digid.get_all_droplets()
+
+
+# In[16]:
+
+#lisdrop = []
+
+
+# In[17]:
+
+#for myd in my_droplets:
+#    print myd
+    #lisdrop.append(myd.image)
+    #
+#    lisdrop.append(myd.ip_address)
+
+
+# In[18]:
+
+#lisdrop
 
 
 # In[ ]:
 
 
+
+
+# In[7]:
+
+#droplet = digitalocean.Droplet(token=tok,
+#                               name='Example',
+#                               region='nyc2', # New York 2
+##                               image= , # Ubuntu 14.04 x64
+ #                              size_slug='512mb',  # 512MB
+#                              backups=True)
+
+
+# In[8]:
+
+#droplet.create()
+
+
+# In[9]:
+
+#dimg = digid.get_all_images()
+
+
+# In[10]:
+
+#for di in dimg:
+##    print di
+
+
+# In[23]:
+
+#opdel = os.listdir('/home/wcmckee/signinlca/deleteusers')
+
+
+# In[24]:
+
+#opdel
+
+
+# In[ ]:
+
+usrtodel = raw_input('Account to delete: ')
+
+
+# In[ ]:
+
+locacc = os.system('sudo passwd -l ' + usrtodel)
+
+
+# In[ ]:
+
+locacc
+
+
+# In[ ]:
+
+os.system('mv ' + '  ' + '/home/wcmckee/signinlca/usernames/' + usrtodel + ' /home/wcmckee/signinlca/username/deleteusers/')
 
