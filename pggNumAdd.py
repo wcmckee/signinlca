535847dcefc7b5d890d184974bf55cd3d0afb7aa
wcmckee <will@artcontrol.me>
2015-02-21 13:52:20 +1300

    roll test

--- pggNumAdd.py
+++ pggNumAdd.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[23]:
 
 # IPython log file
 
@@ -20,7 +20,7 @@ for ranez in range(10):
 
 print ranumlis
 
-savlis = open('/home/wcmckee/pgg/data.json', 'a')      
+savlis = open('/home/wcmckee/pgg/roll.json', 'w')      
 savlis.write(str(ranumlis))
 savlis.close()
 
@@ -33,14 +33,25 @@ savlis.close()
 # 
 # Debian
 
-# In[2]:
+# In[24]:
+
+#for ronum in ranumlis:
+#    print ronum
+
+
+# In[25]:
+
+randict = dict()
+
+
+# In[26]:
 
 othgues = []
 othlow = 0
 othhigh = 9
 
 
-# In[3]:
+# In[27]:
 
 for ranez in range(10):
       randxz = random.randint(othlow, othhigh)
@@ -49,58 +60,105 @@ for ranez in range(10):
       othlow = (othlow + 10)
       othhigh = (othhigh + 10)
 
-print othgues
+#print othgues
 
 
-# In[ ]:
+# In[28]:
 
-dieci = dict()
+tenlis = ['zero', 'ten', 'twenty', 'thirty', 'fourty',
+          'fifty', 'sixty', 'seventy', 'eighty', 
+          'ninety']
 
 
-# In[4]:
+# In[29]:
 
-for ranz in range(10):
-    #print str(ranz) + str(1)
-    dieci.update({str(ranz) + str(1): str(ranz)})
-    for numz in range(10):
+#for telis in tenlis:
+#    for diez in dieci:
+#        print telis
+
+
+# In[30]:
+
+#randict
+
+
+# Makes dict with keys pointing to the 10s numbers. 
+# The value needs the list of random numbers updated.
+# 
+# Currently it just adds the number one.
+# How to add the random number list?
+
+# In[31]:
+
+for ronum in ranumlis:
+    #print ronum
+    if ronum in othgues:
+        print (str(ronum) + ' You Win!')
+    else:
+        print (str(ronum) + ' You Lose!')
+    
+
+
+# In[32]:
+
+#dieci = dict()
+
+
+# In[33]:
+
+#for ranz in range(10):
+    #print str(ranz) + str(1)#
+#    dieci.update({str(ranz) + str(1): str(ranz)})
+#    for numz in range(10):
         #print str(ranz) + str(numz)
-        print numz
+#        print numz
 #print zetoo
 
 
-# In[5]:
+# In[34]:
 
-for sinum  in ranumlis:
-    print str(sinum) + (str('\n'))
+#for diez in dieci:
+#    print diez
+
+
+# In[35]:
+
+#for sinum  in ranumlis:
+#    print str(sinum) + (str('\n'))
     #if str(sinum) in othhigh:
     #    print 'Win'
         
 
 
-# In[6]:
+# In[36]:
 
-import os
+#import os
 
 
-# In[ ]:
+# In[37]:
 
 #os.system('sudo adduser joemanz --disabled-login --quiet -D')
 
 
-# In[ ]:
+# In[38]:
 
-uslis = os.listdir('/home/wcmckee/signinlca/usernames/')
+#uslis = os.listdir('/home/wcmckee/signinlca/usernames/')
 #print ('User List: ')
-for usl in uslis:
-    print usl
-    os.system('sudo adduser ' + usl + ' ' + '--disabled-login --quiet')
+#for usl in uslis:
+#    print usl
+#    os.system('sudo adduser ' + usl + ' ' + '--disabled-login --quiet')
     
-    os.system('sudo mv /home/wcmckee/signinlca/usernames/' + usl  + ' ' + '/home/' + usl + ' ')  
+#    os.system('sudo mv /home/wcmckee/signinlca/usernames/' + usl  + ' ' + '/home/' + usl + ' ')  
 
 
-# In[ ]:
+# In[39]:
 
-print dieci
+#print dieci
+
+
+# In[39]:
+
+
 
 
 # In[ ]:
---
+++ testroll.py
@@ -0,0 +1,227 @@
+
+# coding: utf-8
+
+# In[41]:
+
+from nose.tools import ok_, eq_
+import random
+import re
+#import signinlca
+
+
+# In[41]:
+
+
+
+
+# In[41]:
+
+
+
+
+# In[41]:
+
+
+
+
+# In[42]:
+
+#Nose check dir is there
+#Check username is in usernames folder
+#Check user is in /home/
+
+
+# In[57]:
+
+#print len('erter23')
+
+
+# In[66]:
+
+#Need to nose test raw_input
+#charinp = raw_input('Enter number')
+
+
+# In[67]:
+
+def teslenth():
+    if len(charinp) == 7:
+        assert True
+    else:
+        assert False
+# #   assert len('erter23') == 7
+
+
+# In[ ]:
+
+def childe():
+    
+
+
+# In[ ]:
+
+
+
+
+# In[43]:
+
+#def checkstrlen():
+#    assert len( 'erter23') < 6
+    
+
+
+# In[43]:
+
+
+
+
+# In[44]:
+
+import os
+
+
+# In[45]:
+
+#nose.core.logging.codecs
+
+
+# In[46]:
+
+#nose.run(
+
+
+# In[47]:
+
+#def testhomedir():
+#    assert os.listdir('/home/wcmckee/') == True
+
+
+# Test that the list is empty.
+
+# In[48]:
+
+ranumlis = []
+ranlow = 0
+ranhigh = 9
+randmun = random.randint(ranlow, ranhigh)
+
+
+# In[49]:
+
+#def teststr():
+#    if ranumlis == []:
+#        assert True
+#    else:
+#        assert False
+        
+#def testfullis():
+#    if ranumlis == list:
+#        assert True
+#    else:
+#        assert False
+    
+#def testrandnum():
+#    if randmun == 5:
+#        assert True
+#    else:
+#        assert False
+
+
+# In[50]:
+
+#EMAIL_REGEXP = r'[\S.]+@[\S.]+'
+
+#def test_email_regexp():
+   # a regular e-mail address should match
+#   assert re.match(EMAIL_REGEXP, 'test@nowhere.com')
+
+   # no domain should fail
+#   assert not re.match(EMAIL_REGEXP, 'test@')
+
+
+# In[51]:
+
+#import nose
+
+
+# In[52]:
+
+#nose.tools.assert_greater()
+
+#nose.tools.
+
+
+# In[53]:
+
+#pwd
+
+
+# In[53]:
+
+
+
+
+# In[54]:
+
+#os.listdir('/home/wcmckee/signinlca/')
+os.system('ipython nbconvert /home/wcmckee/github/signinlca/testroll.ipynb --to python')
+
+
+
+# In[10]:
+
+#def testsum():
+#    eq_(4+2,4)
+
+
+# In[13]:
+
+#get_ipython().magic(u'logstart')
+#import random
+#ranumlis = []
+#ranlow = 0
+#ranhigh = 9
+
+#for ranez in range(10):
+#      randmun = random.randint(ranlow, ranhigh)
+#      ranumlis.append(randmun)
+
+#      ranlow = (ranlow + 10)
+#      ranhigh = (ranhigh + 10)
+
+#print ranumlis
+
+
+# In[ ]:
+
+#def testlis():
+    
+
+
+# In[6]:
+
+#def test():
+#    assert False
+
+
+# In[7]:
+
+#def testpass():
+#    pass
+
+
+# In[8]:
+
+#def testevens():
+#    for i in range(0, 5):
+#        yield checkeven, i, i*3
+        
+#def checkeven(n, nn):
+#    assert n % 2 == 0 or nn % 2 == 0
+
+
+# In[ ]:
+
+#def teststr():
+    
+
