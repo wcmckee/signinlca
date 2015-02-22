fe424a2c649c247aab57b6a47c86516463b72b0d
wcmckee <will@artcontrol.me>
2015-02-22 15:42:27 +1300

    nose tests

--- testroll.py
+++ testroll.py
@@ -1,27 +1,25 @@
 
 # coding: utf-8
 
-# In[41]:
+# In[75]:
 
 from nose.tools import ok_, eq_
 import random
 import re
+import nose.tools
 #import signinlca
+import time
+import os
 
 
-# In[41]:
-
-
-
-
-# In[41]:
+# In[ ]:
 
 
 
 
-# In[41]:
+# In[ ]:
 
-
+#nose.tools.assert_list_equal
 
 
 # In[42]:
@@ -44,23 +42,50 @@ import re
 
 # In[67]:
 
-def teslenth():
-    if len(charinp) == 7:
+def testlenth():
+    if len('blahesrs') > 7:
         assert True
     else:
         assert False
+
+#def testchartyp():
+    
 # #   assert len('erter23') == 7
 
 
-# In[ ]:
+# In[73]:
 
-def childe():
+import requests
+import shutil
+
+
+# In[71]:
+
+def testdirmk():
+    if os.path.isdir('/home/wcmckee/github') == True:
+        assert True
+    else:
+        assert False
+    #os.mkdir(gtsdrndir)
     
-
-
-# In[ ]:
-
-
+def testimgur():
+    if 'http://i.imgur.com' in 'http://i.imgur.com/RfIHXaJ.jpg':
+        #print rdz.url
+        assert True
+    else:
+        assert False
+        
+#def testrequeget():
+#    if requests.get('/home/wcmckee/github/signinlca/testroll.py', stream=True):
+        
+    
+        #url = rdz.url
+        #response = requests.get(url, stream=True)
+        #with open(str(rdz.author) + '-reference.png', 'wb') as out_file:
+        #    shutil.copyfileobj(response.raw, out_file)
+        #    del response
+        
+#def test
 
 
 # In[43]:
@@ -68,16 +93,6 @@ def childe():
 #def checkstrlen():
 #    assert len( 'erter23') < 6
     
-
-
-# In[43]:
-
-
-
-
-# In[44]:
-
-import os
 
 
 # In[45]:
@@ -97,14 +112,6 @@ import os
 
 
 # Test that the list is empty.
-
-# In[48]:
-
-ranumlis = []
-ranlow = 0
-ranhigh = 9
-randmun = random.randint(ranlow, ranhigh)
-
 
 # In[49]:
 
@@ -129,14 +136,14 @@ randmun = random.randint(ranlow, ranhigh)
 
 # In[50]:
 
-#EMAIL_REGEXP = r'[\S.]+@[\S.]+'
+EMAIL_REGEXP = r'[\S.]+@[\S.]+'
 
-#def test_email_regexp():
-   # a regular e-mail address should match
-#   assert re.match(EMAIL_REGEXP, 'test@nowhere.com')
+def test_email_regexp():
+#   # a regular e-mail address should match
+   assert re.match(EMAIL_REGEXP, 'test@nowhere.com')
 
-   # no domain should fail
-#   assert not re.match(EMAIL_REGEXP, 'test@')
+#   # no domain should fail
+#    assert not re.match(EMAIL_REGEXP, 'test@')
 
 
 # In[51]:
@@ -151,20 +158,10 @@ randmun = random.randint(ranlow, ranhigh)
 #nose.tools.
 
 
-# In[53]:
-
-#pwd
-
-
-# In[53]:
-
-
-
-
 # In[54]:
 
 #os.listdir('/home/wcmckee/signinlca/')
-os.system('ipython nbconvert /home/wcmckee/github/signinlca/testroll.ipynb --to python')
+#os.system('ipython nbconvert /home/wcmckee/github/signinlca/testroll.ipynb --to python')
 
 
 
@@ -204,24 +201,49 @@ os.system('ipython nbconvert /home/wcmckee/github/signinlca/testroll.ipynb --to 
 #    assert False
 
 
-# In[7]:
+# In[78]:
 
 #def testpass():
 #    pass
 
+#Test counting 0,9 10,19 to 90,99.
 
-# In[8]:
+
+# In[79]:
+
+#def testcountonhundrd():
+#    for coun in range(0,99):
+#        yield coun
+
+
+# In[ ]:
+
+def testodd():
+    for i in range(0,10):
+        yield checkodd, i , i*2
+        
+def checkodd(n, nn):
+    assert n % 3 == 0 or nn % 3 == 0
+        
+
+
+# In[77]:
 
 #def testevens():
-#    for i in range(0, 5):
+#    for i in range(0, 99):
 #        yield checkeven, i, i*3
         
 #def checkeven(n, nn):
 #    assert n % 2 == 0 or nn % 2 == 0
 
 
+# In[76]:
+
+#Test 
+
+
 # In[ ]:
 
-#def teststr():
-    
+#time.sleep(15)
+#os.system('nosetests /home/wcmckee/github/signinlca/testroll.py')
 
