a7134694a60525e885327b8b475ec1c8e7a3bb22
wcmckee <wil@artcontrol.me>
2015-04-29 11:03:51 +1200

    location

--- locrsaky.py
+++ locrsaky.py
@@ -12,62 +12,67 @@
 # 
 # slave machine does rsync to client machine. 
 
-# In[5]:
+# In[28]:
 
-from Crypto.PublicKey import RSA
+#from Crypto.PublicKey import RSA
 import getpass
 import os
 import socket
 
 
-# In[8]:
+# In[29]:
 
 getusr = getpass.getuser()
 
 oshsdir = ('/home/' + getusr + '/.ssh/')
 
 
-# In[6]:
+# In[30]:
 
 gethosz = socket.gethostname()
 
 
-# In[9]:
+# In[31]:
 
-gethosz
+#gethosz
 
-new_key = RSA.generate(2048, e=65537)
-privatekey = new_key.exportKey("PEM")
-publickey = new_key.publickey().exportKey("PEM")
-
-
-# In[11]:
-
-hoezapriv = open(oshsdir + gethosz, 'w')
-
-hoezapriv.write(privatekey)
-
-hoezapriv.close()
-
-hoezapub = open(oshsdir + gethosz + '.pub', 'w')
-
-hoezapub.write(publickey)
-
-hoezapub.close()
+#new_key = RSA.generate(2048, e=65537)
+#privatekey = new_key.exportKey("PEM")
+#publickey = new_key.publickey().exportKey("PEM")
 
 
-# In[ ]:
+# In[32]:
+
+hoezapriv = (oshsdir + gethosz)
+
+#hoezapriv.write(privatekey)
+
+#hoezapriv.close()
+
+#hoezapub = (oshsdir + gethosz + '.pub')
+
+#hoezapub.write(publickey)
+
+#hoezapub.close()
+
+
+# In[33]:
 
 #Need to move the public key to folder where webserver is running.
 #Host machine then picks it up and writes to authorized keys.
 
 
-# In[12]:
+# In[35]:
+
+os.system('ssh-keygen -f ' + gethosz)
+
+
+# In[26]:
 
 bbautdir = ('/home/wcmckee/bbauthkeys/')
 
 
-# In[13]:
+# In[27]:
 
 os.system('cp ' + oshsdir + gethosz + '.pub /home/wcmckee/bbauthkeys/')
 
